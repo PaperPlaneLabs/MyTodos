0f4c24cf0f3e5c25a4600e395ef776cece15466b
SujithChristopher <chrisbon95@gmail.com>
2026-02-17 11:44:59 +0530

    linux build errors are solved

--- src-tauri/src/events/linux.rs
+++ src-tauri/src/events/linux.rs
@@ -4,7 +4,7 @@ use futures_util::stream::StreamExt;
 use std::sync::Arc;
 use tauri::AppHandle;
 use tokio::sync::Mutex;
-use zbus::{Connection, MatchRule};
+use zbus::Connection;
 
 /// Initialize Linux systemd D-Bus listener
 pub fn initialize_linux_listener(app_handle: AppHandle, db: DbConnection) {
@@ -17,40 +17,48 @@ pub fn initialize_linux_listener(app_handle: AppHandle, db: DbConnection) {
         match Connection::system().await {
             Ok(connection) => {
                 // Subscribe to PrepareForSleep signal from systemd
-                let rule = MatchRule::builder()
-                    .msg_type(zbus::message::Type::Signal)
-                    .interface("org.freedesktop.login1.Manager")
-                    .unwrap()
-                    .member("PrepareForSleep")
-                    .unwrap()
-                    .build();
+                // Create a proxy to the login manager
+                match zbus::Proxy::new(
+                    &connection,
+                    "org.freedesktop.login1",
+                    "/org/freedesktop/login1",
+                    "org.freedesktop.login1.Manager",
+                )
+                .await
+                {
+                    Ok(proxy) => {
+                        match proxy.receive_signal("PrepareForSleep").await {
+                            Ok(mut stream) => {
+                                println!(
+                                    "Successfully subscribed to systemd PrepareForSleep signal"
+                                );
 
-                match connection.add_match(rule).await {
-                    Ok(mut stream) => {
-                        println!("Successfully subscribed to systemd PrepareForSleep signal");
+                                while let Some(msg) = stream.next().await {
+                                    // PrepareForSleep signal has a boolean argument:
+                                    // true = about to sleep, false = waking up
+                                    let body = msg.body();
+                                    if let Ok(is_sleeping) = body.deserialize::<bool>() {
+                                        if is_sleeping {
+                                            println!("Linux system suspend detected");
 
-                        while let Some(msg) = stream.next().await {
-                            // PrepareForSleep signal has a boolean argument:
-                            // true = about to sleep, false = waking up
-                            if let Ok(body) = msg.body() {
-                                if let Ok(is_sleeping) = body.deserialize::<bool>() {
-                                    if is_sleeping {
-                                        println!("Linux system suspend detected");
-
-                                        let app = app_handle.lock().await;
-                                        let db = db.lock().await;
-                                        auto_pause_if_running(
-                                            &app,
-                                            &db,
-                                            AutoPauseReason::SystemSleep,
-                                        );
+                                            let app = app_handle.lock().await;
+                                            let db = db.lock().await;
+                                            auto_pause_if_running(
+                                                &app,
+                                                &db,
+                                                AutoPauseReason::SystemSleep,
+                                            );
+                                        }
                                     }
                                 }
+                            }
+                            Err(e) => {
+                                eprintln!("Failed to subscribe to PrepareForSleep signal: {}", e);
                             }
                         }
                     }
                     Err(e) => {
-                        eprintln!("Failed to subscribe to PrepareForSleep signal: {}", e);
+                        eprintln!("Failed to create login1 proxy: {}", e);
                     }
                 }
             }
